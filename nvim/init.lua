4bdd604c71de0b0231d9c38c805091273994e84f
Asep Komarudin <68836805+pojokcode@users.noreply.github.com>
2022-10-05 05:43:03 +0700

    Setting neovim bagian 1

---
+++ nvim/init.lua
@@ -0,0 +1,3 @@
+require('options')
+require('plugins')
+require('colorcheme-config')
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,9 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'navarasu/onedark.nvim'
+
+
+end)
